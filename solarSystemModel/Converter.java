a6fc1b64ec06cf19b08428fcff72e35f1272fe81
Wisekik <45822019+Wisekik@users.noreply.github.com>
2019-03-19 10:05:25 +0100

    Create Converter.java
    Converter from AU to meters

---
+++ solarSystemModel/Converter.java
@@ -0,0 +1,13 @@
+public class Converter {
+    public static void main(String[] args){
+        System.out.println(convert(-3.905962989274014E-01));
+        System.out.println(convert(-1.433029529978211E-01));
+        System.out.println(convert(3.865818553391108E-03));
+        System.out.println(convert(-2.519674903359139E-02));
+    }
+
+    private static double convert(double x){
+        long devider = 149597870700L;
+        return x*devider;
+    }
+}
