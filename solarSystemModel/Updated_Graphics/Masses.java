44fd31d26904df047e6b775908143eef0a9d3a67
Nikolas Oikonomou <n.oikonomou@maastrichtuniversity.nl>
2019-03-21 09:00:53 +0100

    New graphics implementation

---
+++ solarSystemModel/Updated_Graphics/Masses.java
@@ -0,0 +1,98 @@
+//package solarSystemModel;
+
+public abstract class Masses {
+	
+	private final static double mSun = 1.988500E30;
+	private final static double mMercury = 0.33011E24;
+	private final static double mVenus = 4.8675E24;
+	private final static double mEarth = 5.9724E24;
+	private final static double mMars = 0.64171E24;
+	private final static double mJupiter = 1898.19E24;
+	private final static double mSaturn = 568.34E24;
+	private final static double mNeptune = 102.413E24;
+	private final static double mUranus = 86.813E24;
+	private final static double mMoon = 7.34767309E22;
+	private final static double mPhobos = 1.0659E16;
+	private final static double mDeimos = 1.4762E15;
+	private final static double mIo = 8.931938E22;
+	private final static double mEuropa = 4.797E22;
+	private final static double mGanymede = 1.4819E23;
+	private final static double mCallisto = 1.075938E23;
+	private final static double mMimas = 3.7493E19;
+	private final static double mEnceladus = 1.08022E20;
+	private final static double mTethys = 6.17449E20;
+	private final static double mDione = 1.095452E21;
+	private final static double mRhea = 2.306518E21;
+	private final static double mTitan = 1.3452E23;
+	private final static double mIapetus = 1.805635E21;
+
+	public static double getmSun() {
+		return mSun;
+	}
+	public static double getmMercury() {
+		return mMercury;
+	}
+	public static double getmVenus() {
+		return mVenus;
+	}
+	public static double getmEarth() {
+		return mEarth;
+	}
+	public static double getmMars() {
+		return mMars;
+	}
+	public static double getmJupiter() {
+		return mJupiter;
+	}
+	public static double getmSaturn() {
+		return mSaturn;
+	}
+	public static double getmNeptune() {
+		return mNeptune;
+	}
+	public static double getmUranus() {
+		return mUranus;
+	}
+	public static double getmMoon() {
+		return mMoon;
+	}
+	public static double getmPhobos() {
+		return mPhobos;
+	}
+	public static double getmDeimos() {
+		return mDeimos;
+	}
+	public static double getmIo() {
+		return mIo;
+	}
+	public static double getmEuropa() {
+		return mEuropa;
+	}
+	public static double getmGanymede() {
+		return mGanymede;
+	}
+	public static double getmCallisto() {
+		return mCallisto;
+	}
+	public static double getmMimas() {
+		return mMimas;
+	}
+	public static double getmEnceladus() {
+		return mEnceladus;
+	}
+	public static double getmTethys() {
+		return mTethys;
+	}
+	public static double getmDione() {
+		return mDione;
+	}
+	public static double getmRhea() {
+		return mRhea;
+	}
+	public static double getmTitan() {
+		return mTitan;
+	}
+	public static double getmIapetus() {
+		return mIapetus;
+	}
+}
