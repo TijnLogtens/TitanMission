131a87e2a1089f9428e96d76a7a98979f642bfa9
Wisekik <45822019+Wisekik@users.noreply.github.com>
2019-04-12 12:25:14 +0200

    Update CelestialBody.java

--- solarSystemModel/CelestialBody.java
+++ solarSystemModel/CelestialBody.java
@@ -162,12 +162,12 @@ public class CelestialBody{
 
 		for (Object object : planets) {
 			int type = -1;
-			if(object instanceof CelestialBody) {
+			/*if(object instanceof CelestialBody) {
 				CelestialBody planet = (CelestialBody) object;
 				if(planet == this) {
 					double distance = Math.pow((this.x - planet.getX()),2) + Math.pow((this.y - planet.getY()),2) + Math.pow((this.z - planet.getZ()),2);
 					double dist = Math.sqrt(distance);
-					double temp = bigG * planet.getMass() / Math.pow(distance,3);
+					double temp = bigG * planet.getMass() / Math.pow(dist,3);
 
 
 					// Euler acceleration -- ~First Order
@@ -200,34 +200,34 @@ public class CelestialBody{
 					acceleration[1] += (first[1] + 2 * second[1] + 2 * third[1] + fourth[1]);
 					acceleration[2] += (first[2] + 2 * second[2] + 2 * third[2] + fourth[2]);
 				}
-			} else if(object instanceof Star) {
+			} else */if(object instanceof Star) {
 				Star planet = (Star) object;
-				double distance = Math.pow((this.x - planet.getX()),2) + Math.pow((this.y - planet.getY()),2) + Math.pow((this.z - planet.getZ()),2);
+				double distance = Math.pow((this.x),2) + Math.pow((this.y),2) + Math.pow((this.z),2);	
 				double dist = Math.sqrt(distance);
-				double temp = bigG * planet.getMass() / Math.pow(distance,3);
+				double temp = bigG * planet.getMass() / Math.pow(dist,3);
 
 
 				// Euler acceleration -- ~First Order
-				first[0] = temp * (this.x - planet.getX());
-				first[1] = temp * (this.y - planet.getY());
-				first[2] = temp * (this.z - planet.getZ());
+				first[0] = temp * (this.x);
+				first[1] = temp * (this.y);
+				first[2] = temp * (this.z);
 
 				// ~Second Order after a half timestep
-				double[] temp_vel = step(this.vx, this.vy, this.vz, first, dt / 2);
+				double[] temp_vel = step(this.vx, this.vy, this.vz, first, dt/2);
 				double[] temp_position = step(this.x, this.y, this.z, temp_vel, dt / 2);
 				second[0] = planet.getX() - temp_position[0] * temp;
 				second[1] = planet.getY() - temp_position[1] * temp;
 				second[2] = planet.getZ() - temp_position[2] * temp;
 
 				// ~Third Order after a half timestep
-				temp_vel = step(this.vx, this.vy, this.vz, first, dt / 2);
+				temp_vel = step(this.vx, this.vy, this.vz, first, dt/2);
 				temp_position = step(this.x, this.y, this.z, temp_vel, dt / 2);
 				third[0] = planet.getX() - temp_position[0] * temp;
 				third[1] = planet.getY() - temp_position[1] * temp;
 				third[2] = planet.getZ() - temp_position[2] * temp;
 
 				// ~Fourth Order after 1 timestep in the future using
-				temp_vel = step(this.vx, this.vy, this.vz, first, dt / 2);
+				temp_vel = step(this.vx, this.vy, this.vz, first, dt/2);
 				temp_position = step(this.x, this.y, this.z, temp_vel, dt / 2);
 				fourth[0] = planet.getX() - temp_position[0] * temp;
 				fourth[1] = planet.getY() - temp_position[1] * temp;
@@ -236,19 +236,17 @@ public class CelestialBody{
 				acceleration[0] += (first[0] + 2 * second[0] + 2 * third[0] + fourth[0]);
 				acceleration[1] += (first[1] + 2 * second[1] + 2 * third[1] + fourth[1]);
 				acceleration[2] += (first[2] + 2 * second[2] + 2 * third[2] + fourth[2]);
-				System.out.println("X="+acceleration[0]+"	Y= "+ acceleration[1] +"	Z="+ acceleration[2]);
-
-			} else {
+			} /*else {
 				Satellite planet = (Satellite) object;
-				double distance = Math.pow((this.x - planet.getX()),2) + Math.pow((this.y - planet.getY()),2) + Math.pow((this.z - planet.getZ()),2);
+				double distance = Math.pow((this.x - (planet.getX()+planet.getParentPlanet().getX())),2) + Math.pow((this.y - (planet.getY()+planet.getParentPlanet().getY())),2) + Math.pow((this.z - (planet.getZ()+planet.getParentPlanet().getZ())),2);
 				double dist = Math.sqrt(distance);
-				double temp = bigG * planet.getMass() / Math.pow(distance,3);
+				double temp = bigG * planet.getMass() / Math.pow(dist,3);
 
 
 				// Euler acceleration -- ~First Order
-				first[0] = temp * (this.x - planet.getX());
-				first[1] = temp * (this.y - planet.getY());
-				first[2] = temp * (this.z - planet.getZ());
+				first[0] = temp * (this.x - (planet.getX()+planet.getParentPlanet().getX()));
+				first[1] = temp * (this.y - (planet.getY()+planet.getParentPlanet().getY()));
+				first[2] = temp * (this.z - (planet.getZ()+planet.getParentPlanet().getZ()));
 
 				// ~Second Order after a half timestep
 				double[] temp_vel = step(this.vx, this.vy, this.vz, first, dt / 2);
@@ -275,12 +273,13 @@ public class CelestialBody{
 				acceleration[1] += (first[1] + 2 * second[1] + 2 * third[1] + fourth[1]);
 				acceleration[2] += (first[2] + 2 * second[2] + 2 * third[2] + fourth[2]);
 
-			}
+			}*/
+
 			/*if (this != (CelestialBody) object) {
 					CelestialBody planet = (CelestialBody) object;
 					double distance = Math.pow((this.x - planet.getX()),2) + Math.pow((this.y - planet.getY()),2) + Math.pow((this.z - planet.getZ()),2);
 					double dist = Math.sqrt(distance);
-					double temp = bigG * planet.getMass() / Math.pow(distance,3);
+					double temp = bigG * planet.getMass() / Math.pow(dist,3);
 
 
 					// Euler acceleration -- ~First Order
@@ -315,14 +314,15 @@ public class CelestialBody{
 
 				} */
 			}
+		//System.out.println("X="+acceleration[0]+"	Y= "+ acceleration[1] +"	Z="+ acceleration[2]);
 
-		double[] velocity = step(vx, vy, vz, acceleration, dt);
+		double[] velocity = step(this.vx, this.vy, this.vz, acceleration, dt);
 		double[] position = step(this.x, this.y, this.z, velocity, dt);
 
 		this.vx = velocity[0];
 		this.vy = velocity[1];
 		this.vz = velocity[2];
-
+			
 		this.x = position[0];
 		this.y = position[1];
 		this.z = position[2];
@@ -333,9 +333,9 @@ public class CelestialBody{
 	private double[] step(double p1, double p2, double p3, double[] k, double dt){
 		double[] updated = new double[3];
 
-		updated[0] = p1 + k[0] *dt;
-		updated[1] =p2 + k[1] *dt;
-		updated[2] =p3 + k[2] *dt;
+		updated[0] = p1 + k[0] * dt;
+		updated[1] = p2 + k[1] * dt;
+		updated[2] = p3 + k[2] * dt;
 
 		return updated;
 	}
@@ -382,31 +382,24 @@ public class CelestialBody{
 	public void setIndex(int index){
 		this.index = index;
 	}
-
 	public double getSize(){
 		return size;
 	}
-
 	public double getZ() {
 		return z;
 	}
-
 	public void setZ(double z) {
 		this.z = z;
 	}
-
 	public double getVz() {
 		return vz;
 	}
-
 	public void setVz(double vz) {
 		this.vz = vz;
 	}
-
 	public double getBigG() {
 		return bigG;
 	}
-
 	public Star getParent() {
 		return parent;
 	}
