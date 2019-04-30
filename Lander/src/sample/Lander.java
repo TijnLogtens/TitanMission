da7d94b0145896552a43059ebadf8ef867055f65
Nikolas Oikonomou <n.oikonomou@maastrichtuniversity.nl>
2019-04-30 12:54:54 +0200

    Update Function

--- Lander/src/sample/Lander.java
+++ Lander/src/sample/Lander.java
@@ -1,11 +1,72 @@
 package sample;
 
+import java.util.*;
+
 public class Lander {
+
     private double Mass;
     private double x_pos;
     private double y_pos;
     private double x_speed;
     private double y_speed;
+    private final static double g = 1.352;
+    private final static double b = 1;
+    public Lander(double Mass, double x_pos, double y_pos, double x_speed, double y_speed){
+        this.Mass = Mass;
+        this.x_pos  = x_pos;
+        this.y_pos = y_pos;
+        this.x_speed = x_speed;
+    }
 
-    public Lander(double Mass, double x_pos, double y_pos, double x_speed, double y_speed){}
+    public double update(double dt){
+        //Euler's method
+        double firstvy = CalculateVy(dt);
+        y_pos += dt*firstvy;
+        return y_pos;
+    }
+
+    public double CalculateVy(double dt){
+        return -1*(( Mass * g) / b - ( Mass * g) / b * Math.exp((-b)/ Mass * dt));
+    }
+
+
+    public double getMass() {
+        return Mass;
+    }
+
+    public void setMass(double mass) {
+        Mass = mass;
+    }
+
+    public double getX_pos() {
+        return x_pos;
+    }
+
+    public void setX_pos(double x_pos) {
+        this.x_pos = x_pos;
+    }
+
+    public double getY_pos() {
+        return y_pos;
+    }
+
+    public void setY_pos(double y_pos) {
+        this.y_pos = y_pos;
+    }
+
+    public double getX_speed() {
+        return x_speed;
+    }
+
+    public void setX_speed(double x_speed) {
+        this.x_speed = x_speed;
+    }
+
+    public double getY_speed() {
+        return y_speed;
+    }
+
+    public void setY_speed(double y_speed) {
+        this.y_speed = y_speed;
+    }
 }
--- Lander/src/sample/Main.java
+++ Lander/src/sample/Main.java
@@ -8,8 +8,17 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    private static final double GEOSTATIONARY_HEIGHT = ;
+
     @Override
     public void start(Stage primaryStage) throws Exception{
+        Lander lander = new Lander(4771, 0,100000, 0,0);
+        while(lander.update(1) >= 0){
+            System.out.println(lander.update(1));
+        }
+
+
+
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
@@ -17,7 +26,5 @@ public class Main extends Application {
     }
 
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+    public static void main(String[] args) { launch(args);}
 }
