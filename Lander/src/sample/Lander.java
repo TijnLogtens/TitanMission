452d7b245eeb2c867434cf31e0b233f80303601b
nickoik <45833793+nickoik@users.noreply.github.com>
2019-04-30 10:42:06 +0200

    Add files via upload

---
+++ Lander/src/sample/Lander.java
@@ -0,0 +1,11 @@
+package sample;
+
+public class Lander {
+    private double Mass;
+    private double x_pos;
+    private double y_pos;
+    private double x_speed;
+    private double y_speed;
+
+    public Lander(double Mass, double x_pos, double y_pos, double x_speed, double y_speed){}
+}
---
+++ Lander/src/sample/LanderShape.java
@@ -0,0 +1,9 @@
+package sample;
+
+public class LanderShape {
+    private double height;
+    private double width;
+    private double area;
+
+    public LanderShape(double height, double width, double area){}
+}
