c3b0222d4c3ce59d0e33cd2d8f9764454dd34557
Nikolas Oikonomou <n.oikonomou@maastrichtuniversity.nl>
2019-04-30 14:18:40 +0200

    Geostationary

--- Lander/src/sample/Main.java
+++ Lander/src/sample/Main.java
@@ -8,16 +8,18 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    private static final double GEOSTATIONARY_HEIGHT = ;
+    private static final double GEOSTATIONARY_HEIGHT = 72997653.14;
+    private double y;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Lander lander = new Lander(4771, 0,100000, 0,0);
-        while(lander.update(1) >= 0){
-            System.out.println(lander.update(1));
+        Lander lander = new Lander(4771, 0,GEOSTATIONARY_HEIGHT, 0,0);
+
+        do {
+            y =  lander.update(1);
+            System.out.println(y);
         }
-
-
+        while( y >= 0);
 
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
