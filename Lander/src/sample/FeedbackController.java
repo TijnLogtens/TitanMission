b2300ea03f83c07003cb9d884c7eff6c777475ce
Wisekik <45822019+Wisekik@users.noreply.github.com>
2019-05-10 13:05:17 +0200

    Update FeedbackController.java

--- Lander/src/sample/FeedbackController.java
+++ Lander/src/sample/FeedbackController.java
@@ -1,18 +1,38 @@
 package sample;
 
-public class FeedbackController {
+public class FeedbackController implements ControllerInterface {
 
     private double initialVxWind;
     private double initialVyWind;
 
+    private final static double ERROR_MARGIN = 0.1;
 
-    private final static double MAX_WIND_SPEED = 120;
+    /*   controller
+     *   actuator
+     *   plant
+     */
 
-    /*  controller
-        actuator
-        plant
+    /*
+     *   we need thrust as a function of error
+     *   we have 3 thrusters, 2 small (angle), one main (lift)
      */
 
 
 
+    public double directionalThrust(){
+
+
+        return -1;
+    }
+
+    public double liftingThrust(double error){
+        if(error <= ERROR_MARGIN){
+            return 0;
+       /* } else if(vx > 0) {
+            return -1 * error;*/
+        } else {
+            return error;
+        }
+
+    }
 }
