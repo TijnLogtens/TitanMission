0d7af6c17ca5661c7e619d017e2f9416cae924bf
Wisekik <45822019+Wisekik@users.noreply.github.com>
2019-05-21 15:09:29 +0200

    Update Test.java

--- Lander/src/sample/Test.java
+++ Lander/src/sample/Test.java
@@ -9,17 +9,28 @@ public class Test {
         Lander lander = new Lander(4771, 0,GEOSTATIONARY_HEIGHT, 0,0);
         while(true) {
             if (lander.getPosY() > 0) {
-                double newPosition[] = new double[2];
+                double[] newPosition = new double[2];
+                double[] previousPosition = new double[2];
                 for (int i = 0; i < 10; i++) {
                     newPosition = lander.update(1);
+                    previousPosition[0] = lander.getPosX();
+                    previousPosition[1] = lander.getPosY();
                     lander.setPosX(newPosition[0]);
                     lander.setPosY(newPosition[1]);
                 }
                 if (lander.getPosY() < 0) {
-                    System.out.println(iteration++);
+                    System.out.println(getResult(newPosition,previousPosition));
                     lander = new Lander(4771, 0, GEOSTATIONARY_HEIGHT, 0, 0);
                 }
             }
+        }
+    }
+
+    private static double getResult(double[] position, double[] oldPosition){
+        if(Math.abs(position[0]) <= 100 && position[1] - oldPosition[1] < 14){
+            return 1;
+        } else {
+            return 0;
         }
     }
 }
