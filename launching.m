af1a5bd03c990bda6f29014187296ce172dec6ad
Spriha Joshi <sprihajoshi@sprihas-air.wireless.unimaas.local>
2019-06-22 18:35:56 +0200

    update matlab

--- launching.m
+++ launching.m
@@ -1,14 +1,12 @@
 function [v0,angle] = launching(vs,vp,r)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
+% r is the position vector
 vsp=vs-vp;
-magvsp=(vsp(1)^2+vsp(2)^2+vsp(3)^2)^0.5;
+magvsp=(vsp(1)^2+vsp(2)^2+vsp(3)^2)^0.5; %magnitude of vsp
 G=6.67408E-11;
-M=5.972E24;%kg
-r0=6578140;
+M=5.972E24; %mass of earth in kg
+r0=6578140; % distance between earth and sun
 v0=(magvsp^2+2*G*M/r0)^0.5;
-magr=(r(1)^2+r(2)^2+r(3)^2)^0.5;
+magr=(r(1)^2+r(2)^2+r(3)^2)^0.5; %magnitude of r
 magnitude=magr*magvsp;
 angle=acosd((r(1)*vsp(1)+r(2)*vsp(2)+r(3)*vsp(3))/magnitude);
 end
-
